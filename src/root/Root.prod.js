1640f5003f99cf8b8121042b8ddf3b3ec95782cc
hexiao-o <460559761@qq.com>
2017-05-25 23:55:23 +0800

    分离数据

--- src/root/Root.prod.js
+++ src/root/Root.prod.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { hashHistory, browserHistory, Router } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { fromJS } from 'immutable';
 import routes from '../router';
 
 import configureStore from '../store';
+import initData from '../api/initData';
 
-const store = configureStore();
+const initialState = window ? window.__INITDATA__ || initData : {};
+const store = configureStore(fromJS(initialState));
 const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState(state) {
     return state.get('routing').toJS();
