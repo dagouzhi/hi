1391d8814b518b60b6296868e6fe696e8c6e3b9e
hexiao-o <460559761@qq.com>
2017-08-11 10:39:46 +0800

    v1.0.2

--- src/components/GFooter/index.js
+++ src/components/GFooter/index.js
@@ -8,7 +8,14 @@ export default class GFooter extends React.Component {
         <div className="GFooter-l" />
         <div className="GFooter-r" />
         <div className={styles.center}>
-          VERSION: {__VERSION__}
+          <div>工具箱 © 2017-2018 VERSION: {__VERSION__}</div>
+          <p>
+            本网站为非赢利性站点，本网站所有内容均来源于<a href="http://wulihub.pmdaniu.com/">WuliHub</a>，本站不会保存、复制或传播，也不对本站上的任何内容负法律责任。
+          </p>
+          <p>
+            发布的内容仅做react技术开发测试使用，如想了解更多资源可访问<a href="http://wulihub.pmdaniu.com/">WuliHub</a>！
+          </p>
+          <p>若本站侵犯了贵司版权，请给邮箱460559761@qq.com来信，我会及时处理和回复，谢谢！</p>
         </div>
       </div>
     );
